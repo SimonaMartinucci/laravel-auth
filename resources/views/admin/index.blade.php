3211c284de2212111cdd502f9e01754ab27b4fa7
SimonaMartinucci <simo.martinucci3@gmail.com>
2024-09-23 17:14:28 +0200

    feat: add header, aside + create migration, model, seeder

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,26 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
+    <h2 class="mb-4">Benvenuto {{ Auth::user()->name }} nella tua dashboard</h2>
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    <h4>Attualmente sono presenti <b>{{ $count_projects }}</b> progetti</h4>
+
 @endsection
---
+++ resources/views/admin/partials/aside.blade.php
@@ -0,0 +1,15 @@
+<aside class="text-bg-dark py-4">
+    
+    <ul>
+        <li class="mb-3">
+            <a href="#"><i class="fa-solid fa-house"></i> Home</a>
+        </li>
+        <li class="mb-3">
+            <a href="#"><i class="fa-solid fa-list-ul"></i> Elenco progetti</a>
+        </li>
+        <li class="mb-3">
+            <a href="#"><i class="fa-solid fa-square-plus"></i></i> Nuovo progetto</a>
+        </li>
+    </ul>
+
+</aside>
